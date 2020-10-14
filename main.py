e8803104a904c4c2110b1343285c393d3e2a94b8
repl.it user <replituser@example.com>
2020-10-14 15:46:18 +0000

    created main.py to run the code created automatically, updated .replit to run python

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Running script")
+print(" \n \n running Week 1 \n\n")
+import week1.main as w1
+print(" \n \n running Week 2 \n\n")
+import week2.main as week2
+print(" \n \n running Week 3 \n\n")
+import week3.main as w3
+
+
